b769b1caa62e78b0d5d233078f7d121133a26723
4to4 <you@example.com>
2020-12-25 11:41:16 -0500

    Adding more changes

--- practice.py
+++ practice.py
@@ -1,42 +1,91 @@
-g = 10
+import sys
 
-if ( g == 10 ):
-    print(g)
 
-if "egg":
-	print("This is true")
-k=0
-while (g >= k):
-    print(k)
-    k += 1
+def f():
+    str = "Yo_mama"
+    # print("This string is {} {}".format(str,'1'))
+    # print("xyz {2} {1} {0}".format(1, 2, 3))
+    # print(f"This string has {str} values in it")
 
-# string - strings are immutable
- 
-# raw strings
-string1=r'This is raw string with $%R54524'
-print(string1)
+    # print("Dictionaries are perl hashes")
+    # print("Tuples are const arrays")
+    # print("Arrays are list")
+    # print("Sets unordered lists with unique elements, heterogenous")
 
-print(string1[3])
+    d = {"a": 1, "b": 3}
+    print(d)
+    print(
+        dict(
+            enumerate("Where is this string".split(), start=1)
+        )
+    )
+
+    for k, v in enumerate(d):
+        print(f"K{k} V{v}")
+
+    s1 = {1, 2, 2, 3, 3, 3, 3, 3, 35, 5, 5, 5, "A"}
+    print(s1)
+
+    print(1 in s1)
+
+    print('Y') if (1 in s1) else "N"
+
+    l = list([1, 2, 2, 3, 3, 3])
+    print(l[4])
+
+    dict2 = {4: 34, 12: 34, 5: 23434, 1: 65765, 654645: 1}
+    for i in sorted(dict2.keys(), key=int):  # this will sort the list numerically
+        print(f"K:{i}  V:{dict2[i]}")
+
+    # list comprehension
+    words = "Where is my mba I do not see it yet"
+    len_lst = [len(word) for word in words.split() if word != "is"]
+    print(len_lst)
+
+    # dictionary comprehension
+    words = "this is dict comprehension"
+    d_c = {word: len(word) for word in words.split() if len(word) > 2}
+    print(d_c)
+
+    # set comprehension
+    words = "this is set comprehension"
+    s_c = set(
+        len(word) for word in words.split() if len(word) > 2
+    )
+    print(s_c)
+
+    # generator comprehension
+    words = "This is generator comprehension"
+    g_c = (len(word) for word in words.split() if len(word) > 2)
+
+    for i in g_c:
+        print(f"Gen {i}")
+
+    # gen = (len(word) for word in words)
+    # print("Generator comprehension", type(gen), gen)
+    # for i in gen:
+    #     print(f"Gen comprehension Elements {i}")
+    # print("-------------------------------------")
+
+
+if (__name__ == '__main__'):
+    f()
+else:
+    print("No main")
+
+
 
 #bytes in python
 b1 = b'data'
 print(b1)
 
-#lists are mutable
-l1=[1,2,3]
-print(l1)
 
 
-#list constructor
-l1=list("Ritesh")
-print(l1)
+#-------------------------- load json
 
-#dict
-d1 = {1:2, 3:4, 4:5}
-print(d1)
-print(d1[1])
+import json
 
-for k,v in enumerate(d1):
-    print(k, v)
+with open(r'D:\dev\large_dataset\rows.json', encoding='utf-8') as f:
+    data = json.load(f)
 
-#################################################################################
+print(json.dumps(data, indent=2, sort_keys=True))
